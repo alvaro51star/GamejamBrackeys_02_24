8118d29c66d2c6082f8297590d284f587f49d610
alvaro51star <alvaro51star@hotmail.es>
2024-02-15 20:54:59 +0100

    Update PersonDataSO.cs

--- GamejamBrackeys/Assets/_Scripts/ScriptableObjects/PersonDataSO.cs
+++ GamejamBrackeys/Assets/_Scripts/ScriptableObjects/PersonDataSO.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class PersonDataSO : ScriptableObject
 {
     public string _name; //Si esto sobra se quita porque no me quedo claro si lo hacia miriam o no, preguntar después
-    //!JSON que no se de donde viene
+    public TextAsset _dialogueText; 
     public Sprite _sprite;
     public AudioClip _textAudioClip;
     public Sprite _dialogueSprite; //No se si habra asi que lo pongo y listos
