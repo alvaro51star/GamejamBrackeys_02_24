fc2b6b6be6c4ef2fbe256e9b5128f67f56d7a77d
elenarojore <elena.red.kings@gmail.com>
2024-02-17 16:35:54 +0100

    cositas de la escena

--- GamejamBrackeys/Assets/_Scripts/InteractiveObjects/Phone/PhoneFeedback.cs
+++ GamejamBrackeys/Assets/_Scripts/InteractiveObjects/Phone/PhoneFeedback.cs
@@ -1,22 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PhoneFeedback : MonoBehaviour
 {
-    public bool recievingCall;//comprobante por si acasp
+    private bool recievingCall;//comprobante por si acasp
     [SerializeField] private GameObject ringingLightGO;
+    [SerializeField] private PhoneZoomInteractiveObject phoneIntObjScript;
+    [SerializeField] private AudioSource loopAudioSource;
 
     private void Start()
     {
-        EventManager.PhoneRinging?.Invoke();//provisional, deberia invoarlo la puerta       
+        EventManager.PhoneRinging?.Invoke();//provisional, deberia invocarlo la puerta       
     }
 
     private void OnEnable()
     {
         EventManager.PhoneRinging += OnPhoneRinging;
         EventManager.Call += OnCall;
+        EventManager.Call2 += OnCall2;
     }
+
 
     private void OnDisable()
     {
@@ -32,11 +37,20 @@ public class PhoneFeedback : MonoBehaviour
         if (recievingCall)
         {
             ringingLightGO.SetActive(true);//aqui meteriamos la animacion o lo que sea de la luz
+            loopAudioSource.Play();
         }
     }
 
     private void OnCall()
     {
         recievingCall = false;
-    }    
+
+        ringingLightGO.SetActive(false);//aqui meteriamos la animacion o lo que sea de la luz
+        loopAudioSource.Stop();
+    }
+    private void OnCall2()
+    {
+        loopAudioSource.Stop();
+    }
+
 }
