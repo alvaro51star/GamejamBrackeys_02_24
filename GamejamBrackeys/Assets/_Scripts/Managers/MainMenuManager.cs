fc8f06cb817a5fd614bbd004a4e2035a56390d73
elenarojore <elena.red.kings@gmail.com>
2024-02-17 23:22:47 +0100

    fade cambio de escena

--- GamejamBrackeys/Assets/_Scripts/Managers/MainMenuManager.cs
+++ GamejamBrackeys/Assets/_Scripts/Managers/MainMenuManager.cs
@@ -9,11 +9,26 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGame()
     {
+        var nextScene = SceneManager.GetSceneByBuildIndex(1);
+        var nextSceneName = nextScene.name;
+
+        Initiate.Fade(nextSceneName, Color.black, 10);
         SceneManager.LoadScene(1);//game scene
+    }
+
+    public void Credits()
+    {
+        StartCoroutine(ActivateGOAfterTimer(creditsGO));
     }
 
     public void Exit()
     {
         Application.Quit();
+    }
+
+    private IEnumerator ActivateGOAfterTimer(GameObject goToActivate)
+    {
+        yield return new WaitForSeconds(0.2f);
+        goToActivate.SetActive(true);
     }
 }
