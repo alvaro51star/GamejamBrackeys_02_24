af1723979a35c78ce66a96514174c9288c7dab02
alvaro51star <alvaro51star@hotmail.es>
2024-02-18 01:08:33 +0100

    Arreglado error

--- GamejamBrackeys/Assets/_Scripts/Managers/GameManager.cs
+++ GamejamBrackeys/Assets/_Scripts/Managers/GameManager.cs
@@ -4,7 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private int maxPeople = 10;
+    public static GameManager instance;
+    public int badPeopleIn = 0;
+    //[SerializeField] private int maxPeople = 10;
 
-    
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this);
+        else
+            instance = this;
+
+        DontDestroyOnLoad(instance);
+    }
+
+    public void GameOver(int numberOfAltersIn)
+    {
+        switch (numberOfAltersIn)
+        {
+            case 0:
+                Initiate.Fade("0Errors_FinalScene", Color.black, 1f);
+                break;
+            case 1:
+                Initiate.Fade("1Errors_FinalScene", Color.black, 1f);
+                break;
+            case 2:
+                Initiate.Fade("2Errors_FinalScene", Color.black, 1f);
+                break;
+            default:
+                Initiate.Fade("Failed_FinalScene", Color.black, 1f);
+                break;
+        }
+
+        //LLevar al usuario a la escena correspondiente
+    }
 }
